d77a6ab790716a0aa92488d0d9b6ef521aaecf32
Yuna <>
2021-07-07 19:34:15 -0400

    Add equality and identity to Reading

--- reading.py
+++ reading.py
@@ -22,5 +22,33 @@ class Reading:
 
         self.wh = value
 
+    @classmethod
+    def combine(cls, a, b):
+        return cls(
+            a.start_time if a.start_time < b.start_time else b.start_time,
+            a.end_time if a.end_time > b.end_time else b.end_time,
+            "wh",
+            a.wh + b.wh)
+
     def duration(self):
         return self.end_time - self.start_time
+
+    def overlaps(self, other):
+        return ((self.start_time < other.start_time and
+                self.end_time > other.start_time) or
+                (other.start_time < self.start_time and
+                 other.end_time > self.start_time))
+
+    def hash_bucket(self):
+        return self.start_time.date
+
+    def __eq__(self, them):
+        return (isinstance(them, type(self)) and
+                (self.start_time, self.end_time, self.wh) ==
+                (them.start_time, them.end_time, them.wh))
+
+    def __hash__(self):
+        return hash((self.start_time, self.end_time, self.wh))
+
+    def __str__(self):
+        return f"Reading @ {self.start_time} ({self.duration()}): {self.wh} Wh"
--- tests/reading_test.py
+++ tests/reading_test.py
@@ -7,7 +7,7 @@ from reading import Reading
 class TestReading(unittest.TestCase):
     def setUp(self):
         self.sometime = datetime(2010, 12, 25, 14, 30, 0, tzinfo=timezone.utc)
-        self.later = datetime(2010, 12, 25, 14, 30, 1, tzinfo=timezone.utc)
+        self.later = datetime(2010, 12, 26, 14, 30, 0, tzinfo=timezone.utc)
 
     def test_start_time_must_be_before_end_time(self):
         # start and end time the same
@@ -28,4 +28,66 @@ class TestReading(unittest.TestCase):
 
     def test_duration(self):
         r = Reading(self.sometime, self.later, "wh", 1)
-        self.assertEqual(r.duration(), timedelta(seconds=1))
+        self.assertEqual(r.duration(), timedelta(hours=24))
+
+    def test_overlaps(self):
+        t1 = self.sometime
+        t2 = t1 + timedelta(hours=1)
+        t3 = t2 + timedelta(hours=1)
+        t4 = t3 + timedelta(hours=1)
+
+        # one within the other
+        a = Reading(t1, t4, "wh", 1)
+        b = Reading(t2, t3, "wh", 1)
+        self.assertTrue(a.overlaps(b))
+        self.assertTrue(b.overlaps(a))
+
+        # one ends during the other
+        a = Reading(t1, t3, "wh", 1)
+        b = Reading(t2, t4, "wh", 1)
+        self.assertTrue(a.overlaps(b))
+        self.assertTrue(b.overlaps(a))
+
+        # not overlapping
+        a = Reading(t1, t2, "wh", 1)
+        b = Reading(t2, t3, "wh", 1)
+        self.assertFalse(a.overlaps(b))
+        self.assertFalse(b.overlaps(a))
+
+    def test_hash_bucket(self):
+        a = Reading(self.sometime, self.later, "wh", 1)
+        b = Reading(self.sometime, self.later, "wh", 2)
+        c = Reading(self.sometime + timedelta(hours=24),
+                    self.later + timedelta(hours=24), "wh", 1)
+        self.assertEqual(a.hash_bucket(), b.hash_bucket())
+        self.assertNotEqual(a.hash_bucket(), c.hash_bucket())
+
+    def test_equality(self):
+        r = Reading(self.sometime, self.later, "wh", 1)
+
+        # times mismatch
+        bad_time = self.sometime + timedelta(seconds=10)
+
+        other = Reading(bad_time, self.later, "wh", 1)
+        self.assertNotEqual(r, other)
+
+        other = Reading(self.sometime, bad_time, "wh", 1)
+        self.assertNotEqual(r, other)
+
+        # energy mismatch
+        other = Reading(self.sometime, self.later, "wh", 1.0000001)
+        self.assertNotEqual(r, other)
+
+        # fully equal
+        other = Reading(self.sometime, self.later, "wh", 1)
+        self.assertEqual(r, other)
+
+    def test_hashing(self):
+        # unequal
+        a = Reading(self.sometime, self.later, "wh", 1)
+        b = Reading(self.sometime, self.later, "wh", 2)
+        self.assertNotEqual(hash(a), hash(b))
+
+        # equal
+        b = Reading(self.sometime, self.later, "wh", 1)
+        self.assertEqual(hash(a), hash(b))
