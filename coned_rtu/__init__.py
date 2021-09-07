42056b70d007b5cda705652f251fe0abe0a0b1be
Yuna <>
2021-09-07 02:47:19 -0400

    Remove Server lol

--- coned_rtu/__init__.py
+++ coned_rtu/__init__.py
@@ -1,5 +1,4 @@
 # flake8: noqa
 from .coned import Coned, LoginFailedException, json_to_readings
 from .reading import Reading
-from .server import Server
 from .store import BucketList, OverlappingReadingError
--- main.py
+++ main.py
@@ -2,33 +2,24 @@ import sys
 
 from configobj import ConfigObj
 
-from coned_rtu import Coned, Server
-
-# from reading import Reading
+from coned_rtu import Coned
 
 if len(sys.argv) < 2:
     print(f"Usage: {sys.argv[0]} <config file>")
     sys.exit(1)
 config = ConfigObj(sys.argv[1])
 
-SERVER_MODE = config.get("SERVER_MODE", False)
-
 CONED_USER = config.get("CONED_USER")
 CONED_PASS = config.get("CONED_PASS")
 CONED_TOTP = config.get("CONED_TOTP")
-CONED_MAID = config.get("CONED_MAID")
 OPOWER_ACCOUNT_ID = config.get("OPOWER_ACCOUNT_ID")
 OPOWER_METER = config.get("OPOWER_METER")
+CONED_MAID = config.get("CONED_MAID")
 
 coned = Coned(
     CONED_USER, CONED_PASS, CONED_TOTP, OPOWER_ACCOUNT_ID, OPOWER_METER, CONED_MAID
 )
 
-if SERVER_MODE:
-    server = Server(coned)
-    server.record_usage()
-    server.print_readings()
-else:
-    coned.login()
-    usage_json = coned.get_usage()
-    print(usage_json)
+coned.login()
+usage_json = coned.get_usage()
+print(usage_json)
