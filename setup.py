2dd58e5084b638e5cc69458184c638676391833e
Yuna <>
2021-09-07 03:05:00 -0400

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,12 @@ setup(
     description="Getting real-time usage reports from Con Edison",
     packages=["coned_rtu"],
     zip_safe=False,
+    install_requires=[
+        "configobj==5.0.6",
+        "pre-commit==2.14.1",
+        "pyotp==2.3.0",
+        "python-dateutil==2.8.1",
+        "python-dotenv==0.11.0",
+        "selenium==3.141.0",
+    ],
 )
